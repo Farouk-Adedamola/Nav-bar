f55b8da1c66439c884ae0b2b747b6a3b782bcd93
unknown <faroukadedamola1@gmail.com>
2022-10-07 01:11:18 +0100

    changes commited

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,38 +1,43 @@
-import React, { Fragment, useState, useRef, useEffect } from "react";
+import React, { Fragment, useRef, useState, useEffect } from "react";
 import logo from "./logo.svg";
 import { FaBars } from "react-icons/fa";
 import { links, social } from "./data";
 import "./Navbar.css";
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect().height;
+    if (toggle) {
+      linksContainerRef.current.style.height = `${linksHeight}px`;
+    } else {
+      linksContainerRef.current.style.height = "0px";
+    }
+  }, [toggle]);
   return (
     <Fragment>
       <div className="container">
-        <img src={logo} alt="logo" />
-        <div className="bars-btn">
+        <div className="inner-container">
+          <img src={logo} alt="logo" className="logo" />
           <button onClick={() => setToggle(!toggle)} className="bar">
-            <FaBars />
+            <FaBars className="bar-icon" />
           </button>
         </div>
-      </div>
 
-      {toggle && (
-        <div className="menu-list">
-          <div className="list">
-            <ul className="unordered">
-              {links.map((link) => {
-                const { id, url, text } = link;
-                return (
-                  <li key={id}>
-                    <a href={url}>{text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
+        <div className="list" ref={linksContainerRef}>
+          <ul className="unordered" ref={linksRef}>
+            {links.map((link) => {
+              const { id, url, text } = link;
+              return (
+                <li key={id}>
+                  <a href={url}>{text}</a>
+                </li>
+              );
+            })}
+          </ul>
         </div>
-      )}
-      <div className="icon-list">
         <div className="icons">
           <ul className="unordered-icons">
             {social.map((link) => {
